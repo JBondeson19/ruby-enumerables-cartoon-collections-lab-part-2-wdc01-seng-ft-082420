8cc896de4b1e4c917aa308949ccdf7deaf0fe161
James P Bondeson <jpbond58@gmail.com>
2020-08-17 20:20:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,6 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 
   planeteer_calls.find { |valid_calls|  }
-  planeteer_calls.map {valid_calls}
+  planeteer_calls.map {|valid_calls}
 
 end
